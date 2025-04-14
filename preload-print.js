af0e2c47d87c114b39d355914e673d1a4efc3ffb
Equirizon <equirizon@gmail.com>
2025-04-14 17:54:21 +0800

    Make distributables for shipping

--- preload-print.js
+++ preload-print.js
@@ -1,4 +1,4 @@
-const { contextBridge, ipcRenderer } = require('electron');
+import { contextBridge, ipcRenderer } from 'electron';
 
 contextBridge.exposeInMainWorld('electronPrintAPI', {
   onRecordData: (callback) => ipcRenderer.on('record-data', (event, data) => callback(data)),
